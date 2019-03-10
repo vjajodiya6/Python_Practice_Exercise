8bbec02a86fcce365b056d6d6929e3ff43aec312
vjajodiya6 <47888407+vjajodiya6@users.noreply.github.com>
2019-03-10 11:09:09 +0530

    Add files via upload

---
+++ EvenOdd4.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number :"))
+"""if num%4==0:
+    print("The number "+str(num)+" is a multiply of 4")"""
+if num%2==0:
+    print("You have entered an even number i.e :",str(num))
+else:
+    print("You have entered an odd number i.e :",str(num))
